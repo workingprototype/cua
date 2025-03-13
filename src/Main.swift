bd27e7e05e78784a348fd3a5422f7255f50551c3
f-trycua <f@trycua.com>
2025-03-13 22:59:02 +0100

    Update to v0.1.10

--- src/Main.swift
+++ src/Main.swift
@@ -17,7 +17,7 @@ struct Lume: AsyncParsableCommand {
 // MARK: - Version Management
 extension Lume {
     enum Version {
-        static let current: String = "0.1.9"
+        static let current: String = "0.1.10"
     }
 }
 
