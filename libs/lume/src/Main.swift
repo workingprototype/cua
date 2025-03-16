6744de160140669bb0c995867ee5ed272c10ce54
f-trycua <f@trycua.com>
2025-03-16 20:58:27 +0100

    Update to v0.1.11

--- libs/lume/src/Main.swift
+++ libs/lume/src/Main.swift
@@ -17,7 +17,7 @@ struct Lume: AsyncParsableCommand {
 // MARK: - Version Management
 extension Lume {
     enum Version {
-        static let current: String = "0.1.10"
+        static let current: String = "0.1.11"
     }
 }
 
