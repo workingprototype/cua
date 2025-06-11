e68e716f9048434b00db7cdf09cc1d8e2b02cdf6
Morgan Dean <morgandeanhi@gmail.com>
2025-06-11 11:41:07 -0400

    First migration of readme files and doc files to the libraries.

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,10 +1,20 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.shields.io",
+      },
+    ],
+  },
 };
 
 export default withMDX(config);
