b981af8fa52225380f12e3152d9f573b6fdc2409
Morgan Dean <morgandeanhi@gmail.com>
2025-07-21 15:54:19 -0700

    Update tabs -- have it auto-gen

--- docs/src/app/(home)/layout.tsx
+++ docs/src/app/(home)/layout.tsx
@@ -1,28 +1,11 @@
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { CodeXml, Home } from 'lucide-react';
 import type { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout
-      tree={source.pageTree}
-      sidebar={{
-        tabs: [
-          {
-            url: '/home',
-            title: 'Home',
-            icon: <Home className="ml-1" />,
-          },
-          {
-            url: '/api',
-            title: 'API Reference',
-            icon: <CodeXml className="ml-1" />,
-          },
-        ],
-      }}
-      {...baseOptions}>
+    <DocsLayout tree={source.pageTree} {...baseOptions}>
       {children}
     </DocsLayout>
   );
