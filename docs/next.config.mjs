6ca33c13f35650ddafa99190d3ebb7174605d512
Morgan Dean <morgandeanhi@gmail.com>
2025-07-31 11:43:01 +0100

    Specify no trailing slash

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  trailingSlash: false,
   basePath: '/docs',
   async rewrites() {
     return [
